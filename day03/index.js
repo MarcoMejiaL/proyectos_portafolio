a1319fb9e0f4245a8294111b0f62df3f517269d7
Marco Antonio Mejia Lopez <tomatin77@gmail.com>
2021-10-21 12:42:59 -0500

    dia 3 terminado - manipulacion de estilos desde js

---
+++ day03/index.js
@@ -0,0 +1,8 @@
+const inputs = document.querySelectorAll('.controls input');
+
+
+function update (){
+     const sufix =this.dataset.sizing || '';
+     document.documentElement.style.setProperty(`--${this.name}`,this.value + sufix)
+}
+inputs.forEach(input => input.addEventListener('change', update));
